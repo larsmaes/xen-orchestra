5a6bd9b2e003a86a8767754bebd4c96acd0bb919
badrAZ <azizbibadr@gmail.com>
2018-10-30 17:00:43 +0100

    code improvement

--- packages/xo-web/src/xo-app/backup/index.js
+++ packages/xo-web/src/xo-app/backup/index.js
@@ -20,15 +20,13 @@ const DeprecatedMsg = () => (
 
 const MovingRestoreMessage = () => (
   <div className='alert alert-warning'>
-    <Link to='/backup-ng/restore'> {_('moveRestoreLegacyMessage')}</Link>
+    <Link to='/backup-ng/restore'>{_('moveRestoreLegacyMessage')}</Link>
   </div>
 )
 
 const MovingFileRestoreMessage = () => (
   <div className='alert alert-warning'>
-    <Link to='/backup-ng/file-restore'>
-      {_('moveFileRestoreLegacyMessage')}
-    </Link>
+    <Link to='/backup-ng/file-restore'>{_('moveRestoreLegacyMessage')}</Link>
   </div>
 )
 
